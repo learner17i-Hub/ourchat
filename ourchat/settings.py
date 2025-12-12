0486f910e4656c5dbaddab8b86ea936b66b5a467
learner17i <3029906091@qq.com>
2025-12-12 22:22:58 +0800

    修改了发消息会弹报错以及不能直接更新的问题

--- ourchat/settings.py
+++ ourchat/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o_j1$^=7%ca-8p1!#nm990itldesy^xu8o=e_9auvtqe3e1^bp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*",]
 
